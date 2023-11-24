bd3918658dd00d4fefd5d85cf5c10871170040af
Donald Nash <contact.donaldnash@gmail.com>
2023-11-24 00:32:28 -0500

    Implementing Joint Class

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -8,7 +8,12 @@ const MaxWidthWrapper = ({
   children: ReactNode;
 }) => {
   return (
-    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+    <div
+      className={cn(
+        'mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
+        className
+      )}
+    >
       {children}
     </div>
   );
